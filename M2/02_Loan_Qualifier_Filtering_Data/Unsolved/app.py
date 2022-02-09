f4d8371bc50d47d60b0f5e9c2ba59c496465a22e
petepetersen423 <petepetersen423@gmail.com>
2022-02-09 13:16:57 -0800

    Complete exercise 2.1.8

--- M2/02_Loan_Qualifier_Filtering_Data/Unsolved/app.py
+++ M2/02_Loan_Qualifier_Filtering_Data/Unsolved/app.py
@@ -18,7 +18,8 @@ def load_csv(csvpath):
     return data
 
 
-# This function loads a CSV file with the list of banks and available loans information
+# This function loads a CSV file with the list of banks and available loans
+# information
 # from the file defined in `file_path`
 def load_bank_data(file_path):
     csvpath = Path(file_path)
@@ -40,59 +41,69 @@ def calculate_loan_to_value_ratio(loan_amount, home_value):
     loan_to_value_ratio = int(loan_amount) / int(home_value)
     return loan_to_value_ratio
 
+
 # @TODO Define a function that implements the following user story:
 # As a lender,
-# I want to filter the bank list by checking the customer's desired loan against the bank's maximum loan size
-# so that we can know which banks offer the loan amount requested by the customer
+# I want to filter the bank list by checking the customer's desired loan
+#  against the bank's maximum loan size
+# so that we can know which banks offer the loan amount requested by
+#  the customer
 def filter_max_loan_size(loan_amount, bank_list):
     loan_size_approval_list = []
     for bank in bank_list:
-        if loan_amount <= bank_list[1]:
+
+        if int(loan_amount) <= int(bank[1]):
             loan_size_approval_list.append(bank)
     return loan_size_approval_list
-       
-
-
 
 
 # @TODO Define a function that implements the following user story:
 # As a lender,
-# I want to filter the bank list by checking if the customer's credit score is equal to or greater than the minimum allowed credit score defined by the bank
+# I want to filter the bank list by checking if the customer's credit score is
+# equal to or greater than the minimum allowed credit score defined by the bank
 # so that we can know which banks are willing to offer a loan to the customer
 def filter_credit_score(credit_score, bank_list):
 
-loan_min_credit_score_approval_list = []
+    loan_min_credit_score_approval_list = []
     for bank in bank_list:
-        if credit_score <= bank_list[4]:
+        if int(credit_score) <= int(bank[4]):
             loan_min_credit_score_approval_list.append(bank)
     return loan_min_credit_score_approval_list
 
 
 # @TODO Define a function that implements the following user story:
 # As a lender,
-# I want to filter the bank list by comparing if the customer's debt-to-income is equal to or less than the maximum debt-to-income ratio allowed by the bank
-# so that we can assess if the customer will have payment capacity according to the bank's criteria
+# I want to filter the bank list by comparing if the customer's debt-to-income
+# is equal to or less than the maximum debt-to-income ratio allowed by the bank
+# so that we can assess if the customer will have payment capacity according
+# to the bank's criteria
 def filter_debt_to_income(monthly_debt_ratio, bank_list):
     loan_max_DTI_approval_list = []
     for bank in bank_list:
-        if monthly_debt_ratio <= bank_list[3]:
+        if float(monthly_debt_ratio) <= float(bank[3]):
             loan_max_DTI_approval_list.append(bank)
     return loan_max_DTI_approval_list
 
 
-
 # @TODO Define a function that implements the following user story:
 # As a lender,
-# I want to filter the bank list by checking if the customer's loan-to-value is equal to or less than the maximum loan-to-value ratio allowed by the bank
-# so that we assess if the customer's home value is worth as an asset to secure the loan
+# I want to filter the bank list by checking if the customer's loan-to-value
+# is equal to or less than the maximum loan-to-value ratio allowed by the bank
+# so that we assess if the customer's home value is worth as an asset
+# to secure the loan
 def filter_loan_to_value(loan_to_value_ratio, bank_list):
 
+    loan_loan_to_value_ratio_approval_list = []
+    for bank in bank_list:
+        if float(loan_to_value_ratio) <= float(bank[2]):
+            loan_loan_to_value_ratio_approval_list.append(bank)
+    return loan_loan_to_value_ratio_approval_list
 
 
 # This function implements the following user story:
 # As a customer,
-# I want to know what are the best loans in the market according to my financial profile
-# so that I can choose the best option according to my needs
+# I want to know what are the best loans in the market according to my
+# financial profile so that I can choose the best option according to my needs
 def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_value):
     # Calculate the monthly debt ratio
     monthly_debt_ratio = calculate_monthly_debt_ratio(debt, income)
@@ -113,7 +124,8 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
     return bank_data_filtered
 
 
-# This function is the main execution point of the application. It triggers all the business logic.
+# This function is the main execution point of the application. It
+# triggers all the business logic.
 def run():
     # Set the file path of the CVS file with the banks and loans information
     file_path = Path("./data/daily_rate_sheet.csv")
@@ -123,7 +135,8 @@ def run():
     # This print statement will display all of the bank data that is provided.
     # print(f"bank_data: {bank_data}")
 
-    # The following lines, set the applicant's information and implements the following user story:
+    # The following lines, set the applicant's information and implements the
+    # following user story:
     # As a customer,
     # I want to provide my financial information
     # so that I can apply for a loan
