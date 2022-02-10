ce250486d0d454a2d7b7820a61dfc0ddee3d9176
petepetersen423 <petepetersen423@gmail.com>
2022-02-09 21:53:16 -0800

    initial commit for ex 2.3.4

---
+++ M2/cli.py
@@ -0,0 +1,22 @@
+import fire
+import random
+
+
+def clothes_picker(pants=False):
+    shirts_list = [
+        "solid blue t-shirt",
+        "red striped sweatshirt",
+        "purple and green tie dye t-shirt",
+        "black dress shirt",
+    ]
+
+    pants_list = ["Black Dress pants", "Grey sweatpants", "Khakis"]
+
+    if pants:
+        return random.choice(shirts_list), random.choice(pants_list)
+
+    return random.choice(shirts_list)
+
+
+if __name__ == "__main__":
+    fire.Fire(clothes_picker)
