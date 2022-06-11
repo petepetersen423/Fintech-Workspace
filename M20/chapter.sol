b78df8a9a5822bed73f05bd9bcefff03c6803767
Pete Petersen <petepetersen423@gmail.com>
2022-06-11 11:46:18 -0700

    Sol ex

--- M20/chapter.sol
+++ M20/chapter.sol
@@ -1,3 +1,31 @@
 pragma solidity ^0.5.0;
 
 // SPDX-License-Identifier: MIT
+
+contract LatestTrade {
+    string coin = "BTC";
+    uint256 price;
+    bool isBuyOrder;
+
+    function updateTrade(
+        string memory newCoin,
+        uint256 newPrice,
+        bool isBuy
+    ) public {
+        coin = newCoin;
+        price = newPrice;
+        isBuyOrder = isBuy; // Is this a buy or a sell order?
+    }
+
+    function getLatestTrade()
+        public
+        view
+        returns (
+            string memory,
+            uint256,
+            bool
+        )
+    {
+        return (coin, price, isBuyOrder);
+    }
+}
